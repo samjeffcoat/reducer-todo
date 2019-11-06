49055b45cb07d580954e04090f0787607720924a
Sam <Jeffcoat>
2019-11-05 18:32:31 -0600

    Adds logic to case for Toggle Completed

--- reducer-todo/src/reducers/Reducer.js
+++ reducer-todo/src/reducers/Reducer.js
@@ -28,7 +28,17 @@ export const reducer =(state, action) =>{
                     id:Date.now(),
                     completed: false
                 }
-            ]
+            ];
+        case "TOGGLE_COMPLETED":
+            return state.map(todo => {
+                if (todo.id === action.payload){
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo;
+            })
     default:
         return state;
 }}
